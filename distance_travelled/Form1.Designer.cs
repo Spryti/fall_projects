da76929894279aa70004174db7e14d69ca28fb9c
Spryti <jordan.muturi@hgs.hiddengeniusproject.org>
2020-10-14 18:17:48 -0700

    first commit

---
+++ distance_travelled/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Distance_Travelled_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.speedVariable = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.distance = new System.Windows.Forms.Label();
+            this.speedTextBox = new System.Windows.Forms.TextBox();
+            this.productTextBox = new System.Windows.Forms.TextBox();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.timeVariable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate Distance";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // speedVariable
+            // 
+            this.speedVariable.AutoSize = true;
+            this.speedVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speedVariable.Location = new System.Drawing.Point(22, 38);
+            this.speedVariable.Name = "speedVariable";
+            this.speedVariable.Size = new System.Drawing.Size(181, 16);
+            this.speedVariable.TabIndex = 1;
+            this.speedVariable.Text = "Enter Speed (Miles per Hour)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // distance
+            // 
+            this.distance.AutoSize = true;
+            this.distance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.distance.Location = new System.Drawing.Point(78, 163);
+            this.distance.Name = "distance";
+            this.distance.Size = new System.Drawing.Size(119, 16);
+            this.distance.TabIndex = 3;
+            this.distance.Text = "Distance Traveled";
+            // 
+            // speedTextBox
+            // 
+            this.speedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speedTextBox.Location = new System.Drawing.Point(209, 37);
+            this.speedTextBox.Name = "speedTextBox";
+            this.speedTextBox.Size = new System.Drawing.Size(100, 22);
+            this.speedTextBox.TabIndex = 4;
+            // 
+            // productTextBox
+            // 
+            this.productTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productTextBox.Location = new System.Drawing.Point(209, 160);
+            this.productTextBox.Name = "productTextBox";
+            this.productTextBox.Size = new System.Drawing.Size(100, 22);
+            this.productTextBox.TabIndex = 6;
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeTextBox.Location = new System.Drawing.Point(209, 70);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.timeTextBox.TabIndex = 7;
+            // 
+            // timeVariable
+            // 
+            this.timeVariable.AutoSize = true;
+            this.timeVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeVariable.Location = new System.Drawing.Point(64, 71);
+            this.timeVariable.Name = "timeVariable";
+            this.timeVariable.Size = new System.Drawing.Size(133, 16);
+            this.timeVariable.TabIndex = 8;
+            this.timeVariable.Text = "Enter Time (In Hours)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 238);
+            this.Controls.Add(this.timeVariable);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.productTextBox);
+            this.Controls.Add(this.speedTextBox);
+            this.Controls.Add(this.distance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.speedVariable);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label speedVariable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label distance;
+        private System.Windows.Forms.TextBox speedTextBox;
+        private System.Windows.Forms.TextBox productTextBox;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label timeVariable;
+    }
+}
+
